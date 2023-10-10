0d286adefe7b43e17d46a914e54c1102d248a4a7
Markus Laaksonen <markus@mxa.fi>
2023-10-10 19:38:09 +0300

    improve makefile

---
+++ inc/main.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 18:06:41 by malaakso          #+#    #+#             */
+/*   Updated: 2023/10/10 18:20:38 by malaakso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+// Preprocessor definitions
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
+# ifndef WINDOW_WIDTH
+#  define WINDOW_WIDTH 720
+# endif
+# ifndef WINDOW_HEIGHT
+#  define WINDOW_HEIGHT 480
+# endif
+
+// Include system headers
+
+// Include user headers
+# include "../MLX42/include/MLX42/MLX42.h"
+
+#endif
